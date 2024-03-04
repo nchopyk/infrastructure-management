a82d4aaa2d9f74d3639705daa1248a7fe55145cf
Nazar Chopyk <nazar.chopyk@gmail.com>
2024-03-04 17:56:19 +0200

    Fixed CD/CD for terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,12 +5,10 @@ variable "region" {
 
 variable "aws_access_key_id" {
   type      = string
-  sensitive = true
 }
 
 variable "aws_secret_access_key" {
   type      = string
-  sensitive = true
 }
 
 variable "key_pair_name" {
